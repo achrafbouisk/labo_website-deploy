174d2ace7d501706b656762c1c4e5229a2d2dc98
Achraf JS <75223132+achrafbouisk@users.noreply.github.com>
2023-04-16 05:13:27 +0000

    Update urls.py

--- agrilabo/agrilabo_app/urls.py
+++ agrilabo/agrilabo_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.static import serve
+from django.urls import re_path
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,4 +13,7 @@ urlpatterns = [
     path('merci/', views.merci, name='merci'),
     path('login-client/', views.login_client, name='login_client'),
     # path('maintenance/', views.maintenance, name='maintenance'),
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
